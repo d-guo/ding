83491c64c0e3d1505dc687d87fa42ade355a2c69
dguo <dguo@ucsb.edu>
2019-03-19 16:56:58 -0700

    ding works LOL

--- testing.cpp
+++ testing.cpp
@@ -1,11 +1,6 @@
-#include <iostream> 
-#include <windows.h> // WinApi header 
+#include <iostream>
 
-using namespace std;
-
-int main() 
-{ 
-    Beep(523,500); // 523 hertz (C5) for 500 milliseconds     
-    cin.get(); // wait 
-    return 0; 
+int main()
+{
+	std::cout << "\a" << std::endl;
 }
